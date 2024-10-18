461967dec961769ecde03d5769f5a280d4b785ac
Tiago Soares <tsgo27@hotmail.com>
2024-10-18 14:05:20 -0300

    Atualização de código

--- Models/Read-Titulo-Livro.php
+++ Models/Read-Titulo-Livro.php
@@ -2,12 +2,11 @@
 require_once __DIR__ . '/../Config/web-extends.php';
 
 /*
-* Bucar nome título do livro
+* Bucar título do livro
 *
 */
 
 $input = $_POST['input'];
-
 $query = $pdo->prepare("SELECT Titulo, SubTitulo, Situacao FROM obra WHERE LOWER(Titulo) LIKE LOWER(:input) LIMIT 10");
 $query->bindValue(':input', '%' . $input . '%', PDO::PARAM_STR);
 $query->execute();
@@ -21,13 +20,13 @@ if ($results) {
 
         if ($situacao == 'Disponível') {
             $suggestion = '<div class="titulo-suggestion clickable" data-titulo="' . $titulo . '" data-subtitulo="' . $subtitulo . '">
-                            <span style="color: green;">' . $titulo . '</span>
-                           </div>';
+            <span style="color: green;">' . $titulo . '</span>
+            </div>';
         } else {
             // Exibe mensagem indicando que o livro está indisponível
             $suggestion = '<div class="titulo-suggestion">
-                            <span style="color: red;">' . $titulo . ' - Livro Indisponível</span>
-                           </div>';
+            <span style="color: red;">' . $titulo . ' - Livro Indisponível</span>
+            </div>';
         }
 
         echo $suggestion;
