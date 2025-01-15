c4881e51b306cdfc798cf798072dbf8cd7ad5b9a
Tiago Soares <tsgo27@hotmail.com>
2025-01-15 11:16:58 -0300

    Atualização código

--- Config/web_app.php
+++ Config/web_app.php
@@ -1,34 +1,48 @@
 <?php
 /*
-* Load variables 
+* Load environment variables from .env file
 *
 */
 
 function loadEnv($filePath) {
-    if (!file_exists($filePath)) {
-        throw new Exception("O arquivo .env não foi encontrado.");
-    }
-
-    $lines = file($filePath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-    foreach ($lines as $line) {
-        // Ignora linhas de comentários
-        if (strpos(trim($line), '#') === 0) {
-            continue;
+    try {
+        // Verifica se o arquivo .env existe e é legível
+        if (!file_exists($filePath) || !is_readable($filePath)) {
+            throw new Exception("Erro ao carregar arquivo de conexão!.");
         }
 
-        // Verifica se a linha contém um sinal de igual
-        if (strpos($line, '=') === false) {
-            continue;
+        // Lê as linhas do arquivo .env
+        $lines = file($filePath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+        foreach ($lines as $line) {
+            // Ignora linhas de comentários
+            if (strpos(trim($line), '#') === 0) {
+                continue;
+            }
+
+            // Verifica se a linha contém um sinal de igual
+            if (strpos($line, '=') === false) {
+                continue;
+            }
+
+            list($name, $value) = explode('=', $line, 2);
+            $name = trim($name);
+            $value = trim($value);
+
+            // Define uma variável global
+            $_ENV[$name] = $value;
         }
-
-        list($name, $value) = explode('=', $line, 2);
-        $name = trim($name);
-        $value = trim($value);
-
-        // Define uma variável global
-        $_ENV[$name] = $value;
+    } catch (Exception $e) {
+        // Exibe uma mensagem mais genérica e segura
+        echo "Erro ao carregar arquivo de conexão!";
+        // Para fins de depuração ou produção, você pode registrar o erro em um log:
+        // error_log($e->getMessage(), 3, 'path/to/error_log.log');
+        exit();
     }
 }
 
-loadEnv(__DIR__ . '/.env');
+// Caminho do arquivo .env
+$envFilePath = __DIR__ . '/.env';
+
+// Carrega as variáveis do arquivo .env
+loadEnv($envFilePath);
 ?>
